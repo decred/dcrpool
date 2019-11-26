5c99301891dd9efa2a83fb341b39e04dd61adcae
Donald Adu-Poku <donald.adu@gmail.com>
2019-11-26 13:06:47 +0000

    pool: add account tests.

---
+++ pool/account_test.go
@@ -0,0 +1,71 @@
+package pool
+
+import (
+	"fmt"
+	"testing"
+
+	bolt "github.com/coreos/bbolt"
+)
+
+func persistAccount(db *bolt.DB, address string) (*Account, error) {
+	acc, err := NewAccount(address)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create account: %v", err)
+	}
+	err = acc.Create(db)
+	if err != nil {
+		return nil, fmt.Errorf("unable to persist account: %v", err)
+	}
+	return acc, nil
+}
+
+func testAccount(t *testing.T, db *bolt.DB) {
+	accountA, err := persistAccount(db, "DsmQrsCimQ8QBXndN5xLYTRZpWdZpGJNpsh")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	accountB, err := persistAccount(db, "DsornJn4i4cbgQJF3sCQjNUGDi7HZrYcVcc")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch an account with its id.
+	fetchedAccount, err := FetchAccount(db, []byte(accountA.UUID))
+	if err != nil {
+		t.Fatalf("FetchAccount error: %v", err)
+	}
+
+	if fetchedAccount.Address != accountA.Address {
+		t.Fatalf("expected %v as fetched account address, got %v",
+			accountA.Address, fetchedAccount.Address)
+	}
+
+	if fetchedAccount.UUID != accountA.UUID {
+		t.Fatalf("expected %v as fetched account id, got %v",
+			accountA.UUID, fetchedAccount.UUID)
+	}
+
+	// Ensure accounts cannot be updated.
+	err = accountB.Update(db)
+	if err == nil {
+		t.Fatal("expected not supported error")
+	}
+
+	// Delete all accounts.
+	err = accountA.Delete(db)
+	if err != nil {
+		t.Fatalf("delete accountA error: %v ", err)
+	}
+
+	err = accountB.Delete(db)
+	if err != nil {
+		t.Fatalf("delete accountB error: %v ", err)
+	}
+
+	// Ensure the account have been deleted.
+	_, err = FetchAccount(db, []byte(accountA.UUID))
+	if err == nil {
+		t.Fatal("expected no account found error")
+	}
+}
