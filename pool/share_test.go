2cc030001ef3c56abdab036f67bce8b984714214
Donald Adu-Poku <donald.adu@gmail.com>
2019-11-26 13:15:46 +0000

    pool: update share tests.

--- pool/share_test.go
+++ pool/share_test.go
@@ -5,95 +5,18 @@
 package pool
 
 import (
+	"fmt"
 	"math/big"
-	"os"
 	"testing"
 	"time"
 
 	bolt "github.com/coreos/bbolt"
 	"github.com/decred/dcrd/chaincfg/v2"
-	"github.com/decred/dcrd/dcrutil/v2"
 )
 
-var (
-	// TestDB represents the testing database.
-	testDB = "testdb"
-	// Account X address.
-	xAddr = "SsWKp7wtdTZYabYFYSc9cnxhwFEjA5g4pFc"
-
-	// Account X id.
-	xID = ""
-
-	// Account Y address.
-	yAddr = "Ssp7J7TUmi5iPhoQnWYNGQbeGhu6V3otJcS"
-
-	// Account Y id.
-	yID = ""
-
-	// Pool fee address.
-	poolFeeAddrs, _ = dcrutil.DecodeAddress("SsnbEmxCVXskgTHXvf3rEa17NA39qQuGHwQ", chaincfg.SimNetParams())
-)
-
-// setupDB initializes the pool database.
-func setupDB() (*bolt.DB, error) {
-	os.Remove(testDB)
-
-	db, err := openDB(testDB)
-	if err != nil {
-		return nil, err
-	}
-
-	err = createBuckets(db)
-	if err != nil {
-		return nil, err
-	}
-
-	err = upgradeDB(db)
-	if err != nil {
-		return nil, err
-	}
-
-	xID, err = AccountID(xAddr)
-	if err != nil {
-		return nil, err
-	}
-
-	yID, err = AccountID(yAddr)
-	if err != nil {
-		return nil, err
-	}
-
-	err = createPersistedAccount(db, xAddr)
-	if err != nil {
-		return nil, err
-	}
-
-	err = createPersistedAccount(db, yAddr)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, err
-}
-
-// teardownDB closes the connection to the db and deletes the db file.
-func teardownDB(db *bolt.DB) error {
-	err := db.Close()
-	if err != nil {
-		return err
-	}
-
-	err = os.Remove(testDB)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// createPersistedShare creates a share with the provided account, weight
-// and created on time. The share is then persisted to the database.
-func createPersistedShare(db *bolt.DB, account string, weight *big.Rat,
+// persistShare creates a persisted share with the provided account, share
+// weight and creation time.
+func persistShare(db *bolt.DB, account string, weight *big.Rat,
 	createdOnNano int64) error {
 	share := &Share{
 		Account:   account,
@@ -101,75 +24,61 @@ func createPersistedShare(db *bolt.DB, account string, weight *big.Rat,
 		CreatedOn: createdOnNano,
 	}
 
-	return share.Create(db)
-}
-
-// createMultiplePersistedShares creates multiple shares per the count provided.
-func createMultiplePersistedShares(db *bolt.DB, account string, weight *big.Rat,
-	createdOnNano int64, count int) error {
-	for idx := 0; idx < count; idx++ {
-		err := createPersistedShare(db, account, weight, createdOnNano+int64(idx))
-		if err != nil {
-			return err
-		}
+	err := share.Create(db)
+	if err != nil {
+		return fmt.Errorf("unable to persist share: %v", err)
 	}
 
 	return nil
 }
 
-func TestPPSEligibleShares(t *testing.T) {
-	db, err := setupDB()
-	if err != nil {
-		t.Error(err)
-	}
-
-	td := func() {
-		err = teardownDB(db)
-		if err != nil {
-			t.Error(err)
-		}
-	}
-
-	defer td()
-
+func testShares(t *testing.T, db *bolt.DB) {
 	now := time.Now()
-	minNano := now.Add(-(time.Second * 60)).UnixNano()
-	belowMinNano := now.Add(-(time.Second * 80)).UnixNano()
-	maxNano := now.Add(-(time.Second * 30)).UnixNano()
-	aboveMaxNano := now.Add(time.Second * 10).UnixNano()
+	minimumTime := now.Add(-(time.Second * 60)).UnixNano()
+	maximumTime := now.UnixNano()
+	belowMinimumTime := now.Add(-(time.Second * 80)).UnixNano()
+	aboveMaximumTime := now.Add(time.Second * 10).UnixNano()
 	weight := new(big.Rat).SetFloat64(1.0)
-	shareCount := 5
-	expectedShareCount := 10
 
-	err = createMultiplePersistedShares(db, xID, weight, belowMinNano, shareCount)
+	shareCount := 1
+	expectedShareCount := 2
+
+	// Create a share below the PPS time range for account x.
+	err := persistShare(db, xID, weight, belowMinimumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a share at minimum of the PPS time range for account x.
+	err = persistShare(db, xID, weight, minimumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a share at maximum of the PPS time range for account y.
+	err = persistShare(db, yID, weight, maximumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a share above maximum of the PPS time range for account y.
+	err = persistShare(db, yID, weight, aboveMaximumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	minimumTimeBytes := nanoToBigEndianBytes(minimumTime)
+	maximumTimeBytes := nanoToBigEndianBytes(maximumTime)
+
+	// Fetch eligible shares using the minimum and maximum time range.
+	shares, err := PPSEligibleShares(db, minimumTimeBytes, maximumTimeBytes)
 	if err != nil {
 		t.Error(err)
 	}
 
-	err = createMultiplePersistedShares(db, xID, weight, minNano, shareCount)
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = createMultiplePersistedShares(db, yID, weight, maxNano, shareCount)
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = createMultiplePersistedShares(db, yID, weight, aboveMaxNano, shareCount)
-	if err != nil {
-		t.Error(err)
-	}
-
-	minNanoBytes := nanoToBigEndianBytes(minNano)
-	nowNanoBytes := nanoToBigEndianBytes(now.UnixNano())
-	shares, err := PPSEligibleShares(db, minNanoBytes, nowNanoBytes)
-	if err != nil {
-		t.Error(err)
-	}
-
+	// Ensure the returned share count is as expected.
 	if len(shares) != expectedShareCount {
-		t.Errorf("Expected %v eligible PPS shares, got %v",
+		t.Errorf("PPS error: expected %v eligible PPS shares, got %v",
 			expectedShareCount, len(shares))
 	}
 
@@ -185,70 +94,103 @@ func TestPPSEligibleShares(t *testing.T) {
 		}
 	}
 
+	// Ensure account x and account y both have shares returned.
 	if forAccX == 0 || forAccY == 0 {
-		t.Errorf("Expected shares for account X and Y, "+
+		t.Fatalf("PPS error: expected shares for account X and Y, "+
 			"got %v (for x), %v (for y).", forAccX, forAccY)
 	}
 
+	// Ensure account x and account y have equal number of shares.
 	if forAccX != forAccY {
-		t.Errorf("Expected equal shares for account X and Y, "+
+		t.Fatalf("PPS error: expected equal shares for account X and Y, "+
 			"got %v (for x), %v (for y).", forAccX, forAccY)
 	}
 
-	if forAccX != shareCount && forAccY != shareCount {
-		t.Errorf("Expected share counts of %v for account X and Y, "+
+	// Ensure account x and account y have shares equal to the share count.
+	if forAccX != shareCount || forAccY != shareCount {
+		t.Fatalf("PPS error: expected share counts of %v for account X and Y, "+
 			"got %v (for x), %v (for y).", shareCount, forAccX, forAccY)
 	}
-}
 
-func TestPPLNSEligibleShares(t *testing.T) {
-	db, err := setupDB()
+	// Empty the share bucket.
+	err = emptyBucket(db, shareBkt)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("emptyBucket error: %v", err)
 	}
 
-	td := func() {
-		err = teardownDB(db)
-		if err != nil {
-			t.Error(err)
-		}
-	}
-
-	defer td()
-
-	now := time.Now()
-	aboveMinNano := now.Add(-(time.Second * 20)).UnixNano()
-	minNano := now.Add(-(time.Second * 30)).UnixNano()
-	belowMinNano := now.Add(-(time.Second * 40)).UnixNano()
-	weight := new(big.Rat).SetFloat64(1.0)
-	shareCount := 10
-	expectedShareCount := 10
-
-	err = createMultiplePersistedShares(db, xID, weight, belowMinNano,
-		shareCount)
+	// Create a share below the minimum exclusive PPLNS time for account x.
+	err = persistShare(db, xID, weight, belowMinimumTime)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	err = createMultiplePersistedShares(db, yID, weight, aboveMinNano,
-		shareCount)
+	// Create a share below the minimum exxcuive PPLNS time for account y.
+	err = persistShare(db, yID, weight, belowMinimumTime)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	minNanoBytes := nanoToBigEndianBytes(minNano)
-	shares, err := PPLNSEligibleShares(db, minNanoBytes)
+	// Create a share at minimum exclusive PPLNS time for account x.
+	err = persistShare(db, xID, weight, minimumTime)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
+	// Create a share at minimum exclusive PPLNS time for account y.
+	err = persistShare(db, yID, weight, minimumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a share above minimum exclusive PPLNS time for account x.
+	err = persistShare(db, xID, weight, maximumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a share above minimum exclusive PPLNS time for account y.
+	err = persistShare(db, yID, weight, aboveMaximumTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	shares, err = PPLNSEligibleShares(db, minimumTimeBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure the returned number of shates is as expected.
 	if len(shares) != expectedShareCount {
-		t.Errorf("Expected %v eligible PPLNS shares, got %v",
+		t.Errorf("PPLNS error: expected %v eligible PPLNS shares, got %v",
 			expectedShareCount, len(shares))
 	}
+
+	// Ensure account x and account y both have shares returned.
+	if forAccX == 0 || forAccY == 0 {
+		t.Fatalf("PPLNS error: expected shares for account X and Y, "+
+			"got %v (for x), %v (for y).", forAccX, forAccY)
+	}
+
+	// Ensure account x and account y have equal number of shares.
+	if forAccX != forAccY {
+		t.Fatalf("PPLNS error: expected equal shares for account X and Y, "+
+			"got %v (for x), %v (for y).", forAccX, forAccY)
+	}
+
+	// Ensure account x and account y have shares equal to the share count.
+	if forAccX != shareCount || forAccY != shareCount {
+		t.Fatalf("PPLNS error: expected share counts of %v for account X and Y, "+
+			"got %v (for x), %v (for y).", shareCount, forAccX, forAccY)
+	}
+
+	// Empty the share bucket.
+	err = emptyBucket(db, shareBkt)
+	if err != nil {
+		t.Fatalf("emptyBucket error: %v", err)
+	}
 }
 
-func TestCalculateDividend(t *testing.T) {
+func testSharePercentages(t *testing.T) {
 	set := map[string]struct {
 		input  []*Share
 		output map[string]*big.Rat
@@ -322,21 +264,21 @@ func TestCalculateDividend(t *testing.T) {
 			}
 
 			if errCode.String() != expectedCode.String() {
-				t.Errorf("%s: error generated was %v, expected %v.",
+				t.Fatalf("%s: error generated was %v, expected %v.",
 					name, errCode.String(), expectedCode.String())
 			}
 		}
 
 		for account, dividend := range test.output {
 			if actual[account].Cmp(dividend) != 0 {
-				t.Errorf("%s: account %v dividend was %v, "+
+				t.Fatalf("%s: account %v dividend was %v, "+
 					"expected %v.", name, account, actual[account], dividend)
 			}
 		}
 	}
 }
 
-func TestCalculatePoolTarget(t *testing.T) {
+func testCalculatePoolTarget(t *testing.T) {
 	set := []struct {
 		hashRate   *big.Int
 		targetTime *big.Int
@@ -358,89 +300,18 @@ func TestCalculatePoolTarget(t *testing.T) {
 		target, _, err := calculatePoolTarget(chaincfg.MainNetParams(),
 			test.hashRate, test.targetTime)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		expected, success := new(big.Rat).SetString(test.expected)
 		if !success {
-			t.Errorf("failed to parse %v as a big.Int", test.expected)
+			t.Fatalf("failed to parse %v as a big.Int", test.expected)
 		}
 
 		if target.Cmp(expected) != 0 {
-			t.Errorf("for a hashrate of %v and a target time of %v the "+
+			t.Fatalf("for a hashrate of %v and a target time of %v the "+
 				"expected target is %v, got %v", test.hashRate,
 				test.targetTime, expected, target)
 		}
-	}
-}
-
-// Currently not using cursor delete's even though this test passing. An
-// identical test in a PR for dcrwallet is failing, inconsistent
-// behaviour.
-func TestBoltDBCursorDeletion(t *testing.T) {
-	db, err := setupDB()
-	if err != nil {
-		t.Error(err)
-	}
-
-	td := func() {
-		err = teardownDB(db)
-		if err != nil {
-			t.Error(err)
-		}
-	}
-
-	defer td()
-
-	nowNano := time.Now().UnixNano()
-	weight := new(big.Rat).SetFloat64(1.0)
-	shareCount := 10
-
-	err = createMultiplePersistedShares(db, xID, weight, nowNano, shareCount)
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		bkt, err := fetchShareBucket(tx)
-		if err != nil {
-			return err
-		}
-
-		c := bkt.Cursor()
-		iterations := 0
-		for k, _ := c.First(); k != nil; k, _ = c.Next() {
-			iterations++
-		}
-
-		if iterations != shareCount {
-			t.Errorf("Expected to iterate over %v k/v pairs, but "+
-				"iterated %v time(s)", shareCount, iterations)
-		}
-
-		// Test if the iterator can be used to delete each.
-		iterations = 0
-		c = bkt.Cursor()
-		for k, _ := c.First(); k != nil; k, _ = c.Next() {
-			err = c.Delete()
-			if err != nil {
-				return err
-			}
-			iterations++
-		}
-		if iterations != shareCount {
-			t.Errorf("Expected to iterate over and delete %v k/v pairs, "+
-				"but iterated %v time(s)", shareCount, iterations)
-		}
-
-		c = bkt.Cursor()
-		for k, _ := c.First(); k != nil; k, _ = c.Next() {
-			t.Errorf("Expected empty bucket, still has key %x", k)
-		}
-
-		return nil
-	})
-	if err != nil {
-		t.Error(err)
 	}
 }
