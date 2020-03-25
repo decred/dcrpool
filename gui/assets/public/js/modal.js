de6919bfb578621eb7780491fa2d33ba056e9f9f
jholdstock <jholdstock@decred.org>
2020-03-25 13:42:32 +0000

    gui: use VSP style errors for admin password.

--- gui/assets/public/js/modal.js
+++ gui/assets/public/js/modal.js
@@ -9,9 +9,13 @@ $("#adminModal, #accountModal").on('shown.bs.modal', function (e) {
     $(this).find('input').focus();
 });
 
+$("#admin-form").find("input").on("keyup", function(e) { 
+    if (e.key != "Enter") hideError($("#admin-form"));
+});
+
 $("#admin-form").on("submit", function (e) {
     e.preventDefault();
-    
+    hideError($(this));
     $.ajax({
         url: $(this).attr('action'),
         type: $(this).attr('method'),
@@ -20,15 +24,24 @@ $("#admin-form").on("submit", function (e) {
             window.location.replace("/admin");
         },
         error: function(response) {
-            $("#password-error").fadeOut(300, function(){
-                if (response.status === 401) {
-                    $(this).find("p").text("Incorrect password")
-                } else {
-                    $(this).find("p").text("An error occurred")
-                }
-                $(this).fadeIn(300)
-            });
+            if (response.status === 401) {
+                showError($("#admin-form"), "Incorrect password");
+            } else {
+                showError($("#admin-form"), "An error occurred");
+            }
         },
     });
     
 });
+
+function hideError(form) {
+    form.find(".modal-input").css("border-bottom", "1px solid #E6EAED");
+    form.find(".icon-warning").css("opacity", "0");
+    form.find(".err-message").css("opacity", "0");
+}
+
+function showError(form, errorMsg) {
+    form.find(".modal-input").css("border-bottom", "1px solid #ed6d47");
+    form.find(".icon-warning").css("opacity", "1");
+    form.find(".err-message").text(errorMsg).css("opacity", "1");
+}
